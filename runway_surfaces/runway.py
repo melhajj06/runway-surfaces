5395d39d92f5ced1adc333740613026586975da3
melhajj06 <185436964+melhajj06@users.noreply.github.com>
2025-07-09 18:21:22 -0400

    added elevation field to runway class

--- runway_surfaces/runway.py
+++ runway_surfaces/runway.py
@@ -16,14 +16,15 @@ class ApproachTypes(Enum):
 	NON_PRECISION_INSTRUMENT = 1
 
 class Runway:
-	def __init__(self, units, runway_type, approach_type, mid1: tuple, mid2: tuple, length, width, special_surface=False):
+	def __init__(self, units, runway_type, approach_type, mid1: tuple, mid2: tuple, length, width, elevation, special_surface=False):
 		self.units = units
 		self.runway_type = runway_type
 		self.approach_type = approach_type
 		self.pos1 = mid1
 		self.pos2 = mid2
-		self.length = length,
+		self.length = length
 		self.width = width
+		self.elevation = elevation
 		self.special_surface = special_surface
 
 
