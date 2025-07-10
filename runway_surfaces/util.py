38524bdb6bb4a6b9be98bd78b940167059732367
melhajj06 <185436964+melhajj06@users.noreply.github.com>
2025-07-10 13:36:52 -0400

    changed manual translation to numpy translation

--- runway_surfaces/util.py
+++ runway_surfaces/util.py
@@ -1,12 +1,13 @@
 import numpy as np
 from shapely.geometry import LineString
 
+
 def extend_points_in_both_directions(p1: tuple, p2: tuple, amount):
 	t1 = get_higher_point(p1, p2)
 	t2 = get_lower_point(p1, p2)
 
 	p0a = np.array([0, 0])
-	p0b = np.array([t2[0] - t1[0], t2[1] - t1[1]])
+	p0b = np.subtract(t2, t2)  #np.array([t2[0] - t1[0], t2[1] - t1[1]])
 
 	# rotate the points to align with the x axis
 	theta = -np.acos(p0b[0] / np.linalg.norm(p0b))
@@ -31,7 +32,8 @@ def extend_points_in_both_directions(p1: tuple, p2: tuple, amount):
 	p2b = np.matmul(p1b, reverse_rotation_matrix)
 
 	# translate it back to original position
-	return ((p2a[0] + t1[0], p2a[1] + t1[1]), (p2b[0] + t1[0], p2b[1] + t1[1]))
+	return (np.add(p2a, t1), np.add(p2b, t1))  #(p2a[0] + t1[0], p2a[1] + t1[1]), (p2b[0] + t1[0], p2b[1] + t1[1]))
+
 
 def calc_perp_intersection(p1, p2, radius):
 	delta_x = p2[0] - p1[0]
@@ -40,6 +42,7 @@ def calc_perp_intersection(p1, p2, radius):
 	y = (-delta_x / delta_y) * (x - p1[0]) + p1[1]
 
 	return (x, y)
+
 
 def segments_intersect(p1, p2, q1, q2):
 	l1 = LineString([p1, p2])
@@ -50,6 +53,7 @@ def segments_intersect(p1, p2, q1, q2):
 
 def get_higher_point(a, b):
 	return b if b[1] > a[1] else a
+
 
 def get_lower_point(a, b):
 	return b if b[1] < a[1] else a
