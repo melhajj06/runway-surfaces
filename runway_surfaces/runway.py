f5412711bf8cb3de3dd2188fb3bbb4af7ec8b300
melhajj06 <185436964+melhajj06@users.noreply.github.com>
2025-07-11 12:06:46 -0400

    added methods to calculate primary and horizontal surface specifications

--- runway_surfaces/runway.py
+++ runway_surfaces/runway.py
@@ -16,15 +16,59 @@ class ApproachTypes(Enum):
 	VISUAL = 0
 	NON_PRECISION_INSTRUMENT = 1
 
+class RunwayEnd:
+	def __init__(self, point: tuple, approach_type: ApproachTypes):
+		self.point = point
+		self.approach_type = approach_type
+
 
 class Runway:
-	def __init__(self, units, runway_type, approach_type, mid1: tuple, mid2: tuple, width, elevation, special_surface=False):
+	def __init__(self, units: Units, runway_type: RunwayTypes, approach_type: ApproachTypes, end1: RunwayEnd, end2: RunwayEnd, width, elevation, special_surface=False, visibility_minimums=0):
 		self.units = units
 		self.runway_type = runway_type
 		self.approach_type = approach_type
-		self.pos1 = mid1
-		self.pos2 = mid2
-		self.length = np.linalg.norm(np.subtract(mid1, mid2))
+		self.end1 = end1
+		self.end2 = end2
+		self.length = np.linalg.norm(np.subtract(end1.point, end2.point))
 		self.width = width
 		self.elevation = elevation
 		self.special_surface = special_surface
+		self.visiblity_minimums = visibility_minimums
+	
+	
+	def calc_psurface_width(self):
+		visual_only = self.end1.approach_type == ApproachTypes.VISUAL and self.end2.approach_type == ApproachTypes.VISUAL
+		has_npia = self.end1.approach_type == ApproachTypes.NON_PRECISION_INSTRUMENT or self.end2.approach_type == ApproachTypes.NON_PRECISION_INSTRUMENT
+
+		if self.runway_type == RunwayTypes.UTILITY:
+			if visual_only:
+				return 250
+			return 500
+		elif self.runway_type == RunwayTypes.VISUAL:
+			if visual_only:
+				return 500
+		elif self.runway_type == RunwayTypes.NON_PRECISION_INSTRUMENT:
+			if has_npia:
+				if self.visiblity_minimums >= 0.75:
+					return 1000
+			elif self.visiblity_minimums > 0.75:
+				return 500
+			if self.visiblity_minimums > 0.75:
+				return 500
+			elif has_npia and self.visiblity_minimums == 0.75:
+				return 1000
+		elif self.runway_type == RunwayTypes.PRECISION_INSTRUMENT:
+			return 1000
+		
+		# primary surface width was not specificied at this point
+		return 0
+	
+	def calc_hsurface_radius(self):
+		if self.runway_type == RunwayTypes.UTILITY or self.runway_type == RunwayTypes.VISUAL:
+			return 5000
+		
+		return 10000
+		
+			
+
+
