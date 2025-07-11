a104930143a403b60baa409a7da6fa9f8a6f56d4
melhajj06 <185436964+melhajj06@users.noreply.github.com>
2025-07-11 11:45:11 -0400

    created runway info

--- runway_surfaces/runway.py
+++ runway_surfaces/runway.py
@@ -1,11 +1,12 @@
 from enum import Enum
+import numpy as np
 
 class Units(Enum):
 	DEGREES = 0
 	METERS = 1
 	FEET = 2
 
-class RunwayType(Enum):
+class RunwayTypes(Enum):
 	UTILITY = 0
 	VISUAL = 1
 	NON_PRECISION_INSTRUMENT = 2
@@ -15,19 +16,15 @@ class ApproachTypes(Enum):
 	VISUAL = 0
 	NON_PRECISION_INSTRUMENT = 1
 
+
 class Runway:
-	def __init__(self, units, runway_type, approach_type, mid1: tuple, mid2: tuple, length, width, elevation, special_surface=False):
+	def __init__(self, units, runway_type, approach_type, mid1: tuple, mid2: tuple, width, elevation, special_surface=False):
 		self.units = units
 		self.runway_type = runway_type
 		self.approach_type = approach_type
 		self.pos1 = mid1
 		self.pos2 = mid2
-		self.length = length
+		self.length = np.linalg.norm(np.subtract(mid1, mid2))
 		self.width = width
 		self.elevation = elevation
 		self.special_surface = special_surface
-
-
-	
-
-
