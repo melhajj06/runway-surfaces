6bebe1d8bec402cc5f5d2d0cda69bdb7d084becf
melhajj06 <185436964+melhajj06@users.noreply.github.com>
2025-07-15 00:51:31 -0400

    changed separate surface files to single file

--- runway_surfaces/surfaces.py
+++ runway_surfaces/surfaces.py
@@ -23,13 +23,13 @@ def get_horizontal_surface_edges(runways: list[Runway]):
 		return edges
 
 	for runway in runways:
-		point = (runway.end1.point, runway.end2.point)
+		endpoints = (runway.end1.point, runway.end2.point)
 
 		if (runway.special_surface):
 			point = extend_points_in_both_directions(runway.end1.point, runway.end2.point, 200)
 
-		psurface_vertices.append(point[0])
-		psurface_vertices.append(point[1])
+		psurface_vertices.append(endpoints[0])
+		psurface_vertices.append(endpoints[1])
 
 		r = runway.calc_hsurface_radius()
 		radii.append(r)
@@ -50,19 +50,19 @@ def get_horizontal_surface_edges(runways: list[Runway]):
 	# TODO:
 	# check for vertices that would be encompassed entirely by the region had they not been included
 	for i in range(len(reordered_vertices)):
-		prev_vertex = edges[i - 1].p2 if i > 0 else None
+		#prev_vertex = edges[i - 1].p2 if i > 0 else None
 		v1 = reordered_vertices[i]
 		v2 = reordered_vertices[(i + 1) % len(reordered_vertices)]
-		i1 = calc_perp_intersection(v1, v2, reordered_radii[i])
-		i2 = calc_perp_intersection(v2, v1, -reordered_radii[(i + 1) % len(reordered_radii)])
+		#i1 = calc_perp_intersection(v1, v2, reordered_radii[i])
+		#i2 = calc_perp_intersection(v2, v1, -reordered_radii[(i + 1) % len(reordered_radii)])
 
-		if prev_vertex:
-			edges.append(Edge(prev_vertex, i1, arc=True))
+		#if prev_vertex:
+			#edges.append(Edge(prev_vertex, i1, arc=True))
 
-		edges.append(Edge(i1, i2))
+		#edges.append(Edge(i1, i2))
 
-	v1 = edges[len(edges) - 1].p2
-	v2 = edges[0].p1
-	edges.append(Edge(v1, v2, arc=True))
+	#v1 = edges[len(edges) - 1].p2
+	#v2 = edges[0].p1
+	#edges.append(Edge(v1, v2, arc=True))
 
 	return edges
